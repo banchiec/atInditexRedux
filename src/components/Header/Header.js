e1240ac07898887f60dd284b2ff87be42daba3db
banchiec <icaro_19@hotmail.com>
2022-09-15 17:09:22 +0200

    Add event click in logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import { Content, Logo, Menu } from "./headerStyled"
 import { ImMenu } from 'react-icons/im'
 import { BiLogIn } from 'react-icons/bi'
 import { useSelector } from "react-redux"
-import { useEffect } from "react"
+import { Link } from "react-router-dom"
 
 const Header = () => {
 	const {count} = useSelector((state) => state.cart)
@@ -10,7 +10,9 @@ const Header = () => {
 	return(
 		<Content>
 			<Logo>
-				<img src='https://d31dn7nfpuwjnm.cloudfront.net/images/valoraciones/0033/8852/1280px-Vodafone_2017_logo.svg.png?1557142016' alt=""/>
+				<Link to={'/'}>
+					<img src='https://d31dn7nfpuwjnm.cloudfront.net/images/valoraciones/0033/8852/1280px-Vodafone_2017_logo.svg.png?1557142016' alt=""/>
+				</Link>
 			</Logo>
 			<div className='header'>
 				{count}
